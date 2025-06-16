1da47639260c33ff82ba8b4df74d3db87b7f6704
dos-shaman <gmohan2803000@gmail.com>
2025-06-17 04:46:23 +0530

    feat: Sentry implementation and pricing tiers

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
-                        src="/images/logo.svg"
+                        src="/images/logo.png"
                         alt="logo"
                         width={46}
                         height={44}
