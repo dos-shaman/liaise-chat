812fafe590920b1ce433bc2e3a889546cf45a9f5
dos-shaman <gmohan2803000@gmail.com>
2025-06-17 04:00:17 +0530

    feat: added sign-in page

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { subjectsColors, voices } from "@/constants";
+import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+
