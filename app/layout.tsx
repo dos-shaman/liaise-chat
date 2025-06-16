e3dda94c5ed79f64f28219aa1c2db440d6270e5d
dos-shaman <gmohan2803000@gmail.com>
2025-06-17 05:06:27 +0530

    refactor: fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
 import Navbar from "@/components/Navbar";
-
-const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
-});
+import { bricolage } from "@/app/fonts";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Li.AI.se",
@@ -20,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
         <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
           <Navbar />
